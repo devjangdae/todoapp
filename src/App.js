fad77df83fed4d2a6d57132a1d86ca961f54404e
devjangdae <tjqkd0912@gmail.com>
2023-01-14 21:57:29 +0900

    data title test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+const axiosAdd = require('axios').default;
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  
+  useEffect(()=>{
+    axios({
+      method: 'GET',
+      url:'http://localhost:8080/todos',
+    }).then(response => setPosts(response.data.data))
+  }, [])
+  
   return (
     <div className="App">
-      HELLO REACT
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
     </div>
+    
   );
 }
 
