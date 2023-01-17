0b3327af6a85e51df1afb4c1ef260a6714fc1b97
devjangdae <tjqkd0912@gmail.com>
2023-01-17 23:29:51 +0900

    create 페이지, navigate 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Main from './pages/Main';
 import View from './pages/View';
 import Create from './pages/Create';
@@ -12,6 +13,7 @@ function App() {
       <Route path="/view/:id" element={<View />} />
       <Route path="/create" element={<Create />} />
       <Route path="/update" element={<Update />} />
+      <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
     </Routes>
   );
 }
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,61 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const axiosAdd = require('axios').default;
+
 const Create = () => {
-    return (
-      <div>
-        <h1>Create</h1>
-        <p>Create페이지입니다.</p>
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
+
+  const onTitleChange = ((e) => {
+    setTitle(e.target.value);
+  });
+
+  const onContentChange = ((e) => {
+    setContent(e.target.value);
+  });
+
+  const addTodo = () => {
+    if (title === "") {
+      alert("제목을 입력해!");
+    } else {
+      submit();
+      navigate("/main");
+    }
+  }
+
+  const submit = () => {
+    axios.post('http://localhost:8080/todos',
+      {
+        title: title,
+        content: content
+      }
+    )
+      .then((response) => { console.log(response.data); })
+      .catch((response) => {
+        console.log('전송실패');
+      });
+  }
+
+  return (
+    <div>
+      <p>title</p>
+      <p>
+        <input type="text" value={title} onChange={onTitleChange}></input>
+      </p>
+
+      <p>content</p>
+      <p>
+        <textarea onChange={onContentChange}></textarea>
+      </p>
+
+      <div onClick={addTodo}>
+        Create
       </div>
-    );
-  };
-  
-  export default Create;
+    </div>
+  );
+};
+
+export default Create;
